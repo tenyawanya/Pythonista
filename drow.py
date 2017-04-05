94dd8965bf9512c2ac45ddde5ee257aa9f6f7851
Tenyawanya Ikeda <ikd.ipn@gmail.com>
2017-04-05 14:05:07 +0900

    Update drow.py

--- drow.py
+++ drow.py
@@ -3,6 +3,8 @@ from PIL import ImageFont
 from PIL import ImageDraw
 
 import photos
+import webbrowser
+# 画像保存後に instagram を起動させたいので webbrowser も import することにした
 
 # ここを参考に、図形ではなく文字列を任意の場所に重ねられるようにしてみた
 # https://togetter.com/li/1016292
@@ -15,6 +17,7 @@ drawbuffer = ImageDraw.Draw(img1)
 # フォントの指定は iOS 上の full path が必要なので適当なエラーを出させて確かめた
 # ipa フォントは StaSh を使って wget してきて unzip 
 font = ImageFont.truetype('/private/var/mobile/Containers/Shared/AppGroup/たぶんここがそれぞれの端末で違う/Pythonista3/Documents/ipag.ttf', 100)
+# 上の行は端末だけでなく、インストールするたびにユニークなものになるらしいので入れ直した時などは修正が必要
 # 座標は(横,縦)で指定
 drawbuffer.text((20, 20), 'from Pythonista app @tenyawanya', fill='red', font=font )
 
@@ -34,3 +37,6 @@ crop_img.show()
 # ここまで
 if saveit_re is True:
      print ("saved")
+# 保存した画像をそのままインスタに投稿したいので以下を追加
+insta = 'instagram://camera'
+webbrowser.open(insta)
